d77738bb5bd220a536c8276eb84bc531afe5ae83
canbooo <can.bogoclu@gmail.com>
2024-02-13 21:57:17 +0100

    doc: random sampling

--- experiment_design/random_sampling.py
+++ experiment_design/random_sampling.py
@@ -25,6 +25,18 @@ def create(variables: list[Variable], sample_size: int,
            steps: Optional[int] = None,
            scorer: Optional[Scorer] = None
            ) -> np.ndarray:
+    """
+    Create a design of experiments (DoE) by randomly sampling from the
+    variable distributions.
+
+    :param variables: Determines the dimensions of the resulting sample
+    :param sample_size: the number of points to be created
+    :param scorer: Used to rank the generated DoEs. Specifically, steps
+    number of DoEs will be created and the one with the highest score
+    will be returned.
+    :param steps: Number of DoEs to be created to choose the best from
+    :return: DoE matrix with shape (len(variables), samples_size)
+    """
     if steps < 2:
         return _create(variables, sample_size)
 
@@ -44,6 +56,19 @@ def create(variables: list[Variable], sample_size: int,
 def extend(old_sample: np.ndarray, variables: list[Variable],
            sample_size: int, steps: Optional[int] = None,
            scorer: Optional[Scorer] = None):
+    """
+    Extend a design of experiment (DoE) by randomly sampling from the
+    variable distributions.
+
+    :param old_sample: Old DoE matrix with shape (len(variables), old_sample_size)
+    :param variables: Determines the dimensions of the resulting sample
+    :param sample_size: The number of points to be added to the old_sample
+    :param scorer: Used to rank the generated DoEs. Specifically, steps
+    number of DoEs will be created and the one with the highest score
+    will be returned.
+    :param steps: Number of DoEs to be created to choose the best from
+    :return: Matrix of new points to be added with shape (len(variables), samples_size)
+    """
     if steps < 2:
         return _create(variables, sample_size)
 
